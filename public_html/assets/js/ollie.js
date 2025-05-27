f9f3fa0f5e540904a559b577f1c7164468cd7022
lucia9928 <lucia2801salvas@gmail.com>
2025-05-27 14:36:50 +0200

    modified:   public_html/acerca.html 	modified:   public_html/assets/css/ollie.css 	new file:   public_html/assets/imgs/farmacos.jpg 	new file:   public_html/assets/imgs/medicamentos.jpg 	modified:   public_html/assets/js/ollie.js 	modified:   public_html/contacto.html 	modified:   public_html/cronograma.html 	modified:   public_html/index.html 	modified:   public_html/objetivos.html 	modified:   public_html/origen.html 	modified:   public_html/presupuesto.html 	modified:   public_html/recursos.html 	modified:   public_html/seguimiento.html

--- public_html/assets/js/ollie.js
+++ public_html/assets/js/ollie.js
@@ -1,66 +1,94 @@
 /*!
 =========================================================
-* Ollie Landing page
+* Ollie Landing Page - JavaScript personalizado
+* Autor original: DevCRUD (https://devcrud.com)
 =========================================================
-
-* Copyright: 2019 DevCRUD (https://devcrud.com)
-* Licensed: (https://devcrud.com/licenses)
-* Coded by www.devcrud.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
-
-// smooth scroll
-// smooth scroll
-
-
-$(document).ready(function(){
-    $(".navbar .nav-link").on('click', function(event) {
-
-        if (this.hash !== "") {
-
-            event.preventDefault();
-
-            var hash = this.hash;
-
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 700, function(){
-                window.location.hash = hash;
-            });
-        } 
-    });
+$('#img-carousel').owlCarousel({
+    items: 1,
+    loop: true,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true
 });
 
-// portfolio carousel
+$(document).ready(function () {
+
+    // Smooth scroll al hacer clic en enlaces .nav-link (misma página)
+    $(".navbar .nav-link").on('click', function (event) {
+        if (this.hash !== "") {
+            // Verifica si el enlace apunta a otra página o la misma
+            var targetPage = this.pathname.replace(/^\//, '');  // ruta del enlace sin barra inicial
+            var currentPage = window.location.pathname.replace(/^\//, '');  // ruta actual sin barra inicial
+
+            if (targetPage === currentPage) {
+                // Si es la misma página, hacer scroll animado
+                event.preventDefault();
+
+                var hash = this.hash;
+
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 700, function () {
+                    window.location.hash = hash;
+                });
+            } else {
+                // Si es otra página, dejar que el navegador navegue normalmente
+                // (La animación ocurrirá al cargar la nueva página gracias al siguiente bloque)
+            }
+        }
+    });
+
+    // Smooth scroll al cargar la página si hay un hash en la URL (para cuando venimos de otro archivo HTML)
+    if (window.location.hash) {
+        var hash = window.location.hash;
+
+        // Esperar un poco a que todo cargue para animar el scroll
+        setTimeout(function () {
+            // Verifica que el elemento exista en la página
+            if ($(hash).length) {
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 700);
+            }
+        }, 200);
+    }
 $('#owl-portfolio').owlCarousel({
-    margin:30,
-    dots: false,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:false
+    margin: 30,
+    dots: true,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
         },
-        600:{
-            items:3,
-            nav:false
+        600: {
+            items: 1,
+            nav: false
         },
-        1000:{
-            items:4,
-            nav:false,
-            loop:false
+        1000: {
+            items: 1,
+            nav: false
         }
     }
 });
 
-// testmonial carousel
-$('#owl-testmonial').owlCarousel({
-    center: true,
-    items:1,
-    loop:true,
-    nav: true,
-    dots: false
-})
+
+
+
+    // Owl Carousel para testimonios
+    $('#owl-testmonial').owlCarousel({
+        center: true,
+        items: 1,
+        loop: true,
+        nav: true,
+        dots: false
+    });
+
+});
